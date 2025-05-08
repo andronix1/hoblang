ba9423b728e1a8efae071594c871ba7ada461f15
andronix1 <andronix1.dev@gmail.com>
2025-05-08 15:23:10 +0700

    test: better parse api

--- src/parser/api.test.c
+++ src/parser/api.test.c
@@ -12,12 +12,9 @@
 #include <CUnit/CUnit.h>
 #include <string.h>
 
-typedef struct {
-    AstType *i32;
-    AstType *mod_i64;
-} TestTypes;
+static Mempool *mempool;
 
-static AstType *create_type(Mempool *mempool, const char *str) {
+static AstType *create_type(const char *str) {
     AstPathSegment *segments = vec_new_in(mempool, AstPathSegment);
     size_t begin = 0;
     Slice slice = slice_from_cstr(str);
@@ -46,42 +43,37 @@ static void check_parsing(AstNode **expects, const char *code) {
 }
 
 static void test_parser_type_decl_ident() {
-    Mempool *mempool = mempool_new(1024);
     check_parsing(
         vec_create_in(mempool,
                 ast_node_new_type_decl(mempool,
                     slice_from_cstr("MyCustomType"),
-                    create_type(mempool, "super")
+                    create_type("super")
                 )
             ),
         "type MyCustomType = super"
     );
-    mempool_free(mempool);
 }
 
 static void test_parser_type_decl_path() {
-    Mempool *mempool = mempool_new(1024);
     check_parsing(
         vec_create_in(mempool,
                 ast_node_new_type_decl(mempool,
                     slice_from_cstr("MyCustomType"),
-                    create_type(mempool, "super.cool")
+                    create_type("super.cool")
                 )
             ),
         "type MyCustomType = super.cool"
     );
-    mempool_free(mempool);
 }
 
 static void test_parser_type_decl_struct_with_trailing_comma() {
-    Mempool *mempool = mempool_new(1024);
     check_parsing(
         vec_create_in(mempool,
                 ast_node_new_type_decl(mempool,
                     slice_from_cstr("MyCustomType"),
                     ast_type_new_struct(mempool, vec_create_in(mempool,
-                        ast_struct_field_new(slice_from_cstr("a"), create_type(mempool, "i32")),
-                        ast_struct_field_new(slice_from_cstr("b"), create_type(mempool, "mod.i64"))
+                        ast_struct_field_new(slice_from_cstr("a"), create_type("i32")),
+                        ast_struct_field_new(slice_from_cstr("b"), create_type("mod.i64"))
                     )))
             ),
         "type MyCustomType = struct {\n"
@@ -89,18 +81,16 @@ static void test_parser_type_decl_struct_with_trailing_comma() {
         "    b: mod.i64,\n"
         "}"
     );
-    mempool_free(mempool);
 }
 
 static void test_parser_type_decl_struct() {
-    Mempool *mempool = mempool_new(1024);
     check_parsing(
         vec_create_in(mempool,
                 ast_node_new_type_decl(mempool,
                     slice_from_cstr("MyCustomType"),
                     ast_type_new_struct(mempool, vec_create_in(mempool,
-                        ast_struct_field_new(slice_from_cstr("a"), create_type(mempool, "i32")),
-                        ast_struct_field_new(slice_from_cstr("b"), create_type(mempool, "mod.i64"))
+                        ast_struct_field_new(slice_from_cstr("a"), create_type("i32")),
+                        ast_struct_field_new(slice_from_cstr("b"), create_type("mod.i64"))
                     )))
             ),
         "type MyCustomType = struct {\n"
@@ -108,13 +98,40 @@ static void test_parser_type_decl_struct() {
         "    b: mod.i64\n"
         "}"
     );
+}
+
+static void test_parser_multiple_nodes() {
+    check_parsing(
+        vec_create_in(mempool,
+                ast_node_new_type_decl(mempool,
+                    slice_from_cstr("Type1"),
+                    create_type("t1")
+                ),
+                ast_node_new_type_decl(mempool,
+                    slice_from_cstr("Type2"),
+                    create_type("t2")
+                )
+            ),
+        "type Type1 = t1\n"
+        "type Type2 = t2\n"
+    );
+}
+
+static int on_test_setup() {
+    mempool = mempool_new(1024);
+    return 0;
+}
+
+static int on_test_end() {
     mempool_free(mempool);
+    return 0;
 }
 
 void test_parser() {
-    CU_pSuite suite = CU_add_suite("lexer", NULL, NULL);
+    CU_pSuite suite = CU_add_suite("lexer", on_test_setup, on_test_end);
     CU_ADD_TEST(suite, test_parser_type_decl_ident);
     CU_ADD_TEST(suite, test_parser_type_decl_path);
     CU_ADD_TEST(suite, test_parser_type_decl_struct);
     CU_ADD_TEST(suite, test_parser_type_decl_struct_with_trailing_comma);
+    CU_ADD_TEST(suite, test_parser_multiple_nodes);
 }
