8461b9799f31d6909bcacc593b021d90a7edf83c
andronix1 <andronix1.dev@gmail.com>
2025-06-23 11:49:33 +0700

    fix: defer execution before return statement

--- src/sema/module/stmts/return.c
+++ src/sema/module/stmts/return.c
@@ -1,5 +1,7 @@
 #include "return.h"
+#include "core/mempool.h"
 #include "core/null.h"
+#include "hir/api/expr.h"
 #include "sema/module/module.h"
 #include "sema/module/scope.h"
 #include "sema/module/exprs/expr.h"
@@ -22,8 +24,13 @@ bool sema_module_emit_stmt_return(SemaModule *module, AstReturn *ret) {
         sema_module_err(module, ret->value->slice, "expected to return $t, got $t", module->ss->returns, runtime->type);
         return false;
     }
+    HirLocalId local_id = hir_fun_add_local(module->hir, module->ss->func_id, hir_func_local_new(
+        sema_type_hir_id(runtime->type), HIR_IMMUTABLE));
+    sema_ss_append_stmt(module->ss, hir_stmt_new_init_final(local_id, hir_expr_new(output.steps)));
     sema_module_emit_defers(module);
-    sema_ss_append_stmt(module->ss, hir_stmt_new_ret(hir_expr_new(output.steps)));
+    sema_ss_append_stmt(module->ss, hir_stmt_new_ret(hir_expr_new(vec_create_in(module->mempool,
+        hir_expr_step_new_get_local(local_id)
+    ))));
     sema_module_scope_break(module);
     return true;
 }
