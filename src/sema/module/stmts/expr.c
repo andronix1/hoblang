fa1f28e6d3d7c55fbf49b005ddbae09881f27d89
andronix1 <andronix1.dev@gmail.com>
2025-07-08 11:16:34 +0700

    fix(sema): emit defers before finish function

--- src/sema/module/stmts/expr.c
+++ src/sema/module/stmts/expr.c
@@ -6,6 +6,9 @@
 bool sema_module_emit_stmt_expr(SemaModule *module, AstExpr *expr) {
     SemaExprOutput output = sema_expr_output_new(module->mempool);
     SemaValue *value = NOT_NULL(sema_module_emit_expr(module, expr, sema_expr_ctx_new(&output, NULL)));
+    if (sema_value_is_finish(value)) {
+        sema_module_emit_defers(module);
+    }
     sema_ss_append_stmt(module->ss, hir_stmt_new_expr(sema_expr_output_collect(&output)));
     if (sema_value_is_finish(value)) {
         sema_module_scope_break(module);
